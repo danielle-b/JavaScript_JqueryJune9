2c49f0f04ef2ed9c466792e7ae36b0444ace3e5c
Danielle Brantley <danielle.brant24@gmail.com>
2015-06-16 17:53:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//FOR LOOPS
+for (i = 0; i < 10; i++) {
+	  console.log(i);
+}
+
+
+for (x = 0; x < 20; i--) {
+	console.log(x);
+}
+
+
+var scores = [25, 37, 19, 42, 58];
+for (var i = 0; i > scores; i++){
+   console.log(scores[i])
+}
+
+//WHILE LOOPS
+while (x = 100){
+	console.log(x + "bear");
+	x++;
+}
+
+var number = 0
+while (number <= 20){
+	console.log(number);
+	number = number + 2;
+}
+
+var result = 1
+var counter = 0
+while (counter < 50){
+	result = result * 2;
+	counter = counter + 1;
+}
